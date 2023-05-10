22f4745d1c7158777d89bce90c741c10c90d2bef
Igor Laptop <you@example.com>
2023-05-10 12:44:42 +0300

    Rewrote views

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LoginView, LogoutView
@@ -9,12 +9,14 @@ from main_app.models import Product, Purchase, User
 from main_app.forms import ProductForm, PurchaseForm, UserCreateForm, UserForm
 
 
-class ProductListView(TemplateView):
+class ProductListView(ListView):
     template_name = 'home_page.html'
-    extra_context = {'products': Product.objects.all()}
+    model = Product
+    paginate_by = 3
 
 
 class ProductCreateView(LoginRequiredMixin, CreateView):
+    login_url = '/'
     form_class = ProductForm
     template_name = 'product_add.html'
     success_url = '/'
@@ -22,10 +24,10 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
     login_url = 'login/'
+    model = Product
     form_class = ProductForm
     template_name = 'product_update.html'
     success_url = '/'
-    queryset = Product.objects.all()
 
 
 class ProductDetailView(DetailView):
@@ -40,15 +42,15 @@ class ProductDetailView(DetailView):
         return context
 
 
-class ProfileView(LoginRequiredMixin, TemplateView):
+class ProfileView(LoginRequiredMixin, ListView):
     login_url = 'login/'
     template_name = 'profile.html'
-    # extra_context = {'purchase': Purchase.objects.filter(user=self.request.user)}
+    queryset = Purchase.objects.all()
+    paginate_by = 5
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['purchase'] = Purchase.objects.filter(user=self.request.user)
-        return context
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
 
 
 class PurchaseCreateView(CreateView):
