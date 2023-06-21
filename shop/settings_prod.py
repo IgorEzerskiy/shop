a902e4e7f63b68019fbf9b2eccd977b01e8349c2
VladyslavPonomaryov <vladyslav.ponomaryov@ab-inbev.com>
2023-06-21 19:57:07 +0200

    Change settings to collectstatic

--- shop/settings_prod.py
+++ shop/settings_prod.py
@@ -13,3 +13,21 @@ DATABASES = {
         'PORT': os.environ.get('DBPORT', '5432'),
     }
 }
+
+
+# Optional
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
+# Required
+AWS_STORAGE_BUCKET_NAME = 'py5o'
+AWS_S3_REGION_NAME = 'us-east-1'  # e.g. us-east-2
+AWS_ACCESS_KEY_ID = os.environ.get('KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('ACCESS_KEY')
+# НЕ ВПИСЫВАЙТЕ САМИ КЛЮЧИ, ТОЛЬКО os.environ.get('SOME_KEY')
+
+# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.StaticStorage'
